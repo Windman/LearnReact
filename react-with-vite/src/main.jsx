a941d1905a0b36c4e0895b9f19bd0bfabee90339
ROOT\mkilovatiy <mkilovatiy@intermedia.com>
2024-07-10 16:20:09 +0400

    Render restaurants

--- react-with-vite/src/main.jsx
+++ react-with-vite/src/main.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { restaurants } from "./materials/mocks";
 
-const rootElment = document.getElementById('root');
+const rootElment = document.getElementById("root");
 const reactRoot = ReactDOM.createRoot(rootElment);
-reactRoot.render("hello");
-  
 
+const data = restaurants.map((item) => {
+  const template = (
+    <div className="restaurant-card" key={item.id}>
+      <div className="restaurant-card__name">
+        <h1>{item.name}</h1>
+      </div>
+      <div className="restaurant-card__menu">
+        <h3>Menu</h3>
+        <ul className="restaurant-card__menu-list">
+          {item.menu.map((menuItem) => (
+            <li key={menuItem.id}>{menuItem.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="restaurant-card__reviews">
+        <h3>Reviews</h3>
+        <ul className="restaurant-card__review-list">
+          {item.reviews.map((reviewItem) => (
+            <li key={reviewItem.id}>{reviewItem.text}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+
+  return template;
+});
+
+reactRoot.render(data);
