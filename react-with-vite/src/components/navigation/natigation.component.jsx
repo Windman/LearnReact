22d8efa241ae110893f537307e5c94d7f1d3841f
ROOT\mkilovatiy <mkilovatiy@intermedia.com>
2024-07-17 17:14:00 +0400

    Remove warnings

--- react-with-vite/src/components/navigation/natigation.component.jsx
+++ react-with-vite/src/components/navigation/natigation.component.jsx
@@ -2,7 +2,7 @@ export const Navigation = ({ items, selectedIndex=0, onSelect, type="TAB" }) => 
   return (
     <div className="navigation">
       {items.map((item, index) => (
-        <div className="navigation-item">
+        <div className="navigation-item" key={index}>
           <button onClick={() => onSelect(index)}>{item.name}</button>
         </div>
       ))}
